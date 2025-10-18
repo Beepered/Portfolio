314d55078571912fd7e8c535becba0c2cb60535f
Beepered <bktrieu@gmail.com>
2025-10-17 22:31:09 -0700

    idiots on screen

--- src/components/BottomBar/Playground/characters/Dummy.js
+++ src/components/BottomBar/Playground/characters/Dummy.js
@@ -1,21 +1,39 @@
-export class Player extends Phaser.Physics.Arcade.Sprite {
+export class Dummy extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y, gameSize) {
-    let texture = "Player";
+    let texture = "Dummy";
     super(scene, x, y, texture);
     this.scene.add.existing(this);
 
     this.gameSize = gameSize;
     this.moveSpeed = 0.1;
+    this.setInteractive();
 
-    /*
+    this.sounds = [];
+    for (let i = 1; i <= 4; i++) {
+      this.sounds.push(
+        scene.sound.add(texture + i, {
+          volume: 0.75,
+        })
+      );
+    }
+
     this.anims.create({
       key: "idle",
       frames: this.anims.generateFrameNumbers(texture, {
+        frameRate: 8,
         start: 0,
         end: 2,
       }),
     });
-    */
+    this.anims.create({
+      key: "walk",
+      frames: this.anims.generateFrameNumbers(texture, {
+        frameRate: 8,
+        start: 3,
+        end: 5,
+      }),
+    });
+
     this.States = {
       IDLE: "idle",
       WALK: "walk",
@@ -24,12 +42,18 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 
     this.idleMin = 1;
     this.idleMax = 3;
-    this.walkMin = 2;
-    this.walkMax = 3;
+    this.walkMin = 1;
+    this.walkMax = 3.5;
     this.time = Phaser.Math.Between(this.idleMin, this.idleMax);
 
     this.walkX = 0;
     this.walkY = 0;
+
+    this.on("pointerdown", () => this.OnClick());
+  }
+
+  OnClick() {
+    this.sounds[Phaser.Math.Between(0, this.sounds.length - 1)].play();
   }
 
   update(time, delta) {
@@ -43,9 +67,10 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 
   Idle() {
     // idle animation
+    this.play("idle", true);
     if (this.time <= 0) {
-      this.walkX = Phaser.Math.Between(-1, 1); // FloatBetween
-      this.walkY = Phaser.Math.Between(-1, 1);
+      this.walkX = Phaser.Math.FloatBetween(-1, 1);
+      this.walkY = Phaser.Math.FloatBetween(-1, 1);
 
       this.time = Phaser.Math.Between(this.idleMin, this.idleMax);
       this.state = this.States.WALK;
@@ -53,8 +78,11 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
   }
 
   Walk(walkX, walkY, delta) {
+    this.play("walk", true);
     this.x += walkX * this.moveSpeed * delta;
     this.y += walkY * this.moveSpeed * delta;
+
+    this.flipX = this.walkX < 0; // flip if walk backwards
 
     // check if at sides of screen
     if (this.x <= this.width / 2 + 5) {
--- src/components/BottomBar/Playground/scenes/Playground.js
+++ src/components/BottomBar/Playground/scenes/Playground.js
@@ -3,11 +3,11 @@ import { Scene } from "phaser";
 
 // characters
 // (texture, import, and class name must all be the same)
-import { Player } from "@characters/Player.js";
+import { Dummy } from "@characters/Dummy.js";
 
 const NUM_CHAR = 3;
 
-let possibleChar = [Player];
+let possibleChar = [Dummy];
 let chosenChar = [];
 
 export class PlayGround extends Scene {
@@ -18,8 +18,21 @@ export class PlayGround extends Scene {
   preload() {
     // load images for chosen characters
     for (let i = 0; i < NUM_CHAR; i++) {
-      let char = possibleChar[0];
-      this.load.image(char.name, "assets/" + char.name + ".png");
+      let char = possibleChar[Phaser.Math.Between(0, possibleChar.length - 1)];
+      this.load.spritesheet(char.name, "assets/sprites/" + char.name + ".png", {
+        // I know it isn't very OOP of me, but I just want funny characters walking around
+        frameWidth: 32,
+        frameHeight: 32,
+      });
+
+      // load audio
+      for (let i = 1; i <= 4; i++) {
+        this.load.audio(
+          char.name + i,
+          "assets/sounds/" + char.name + "/" + char.name + i + ".wav"
+        );
+      }
+
       chosenChar.push(char);
     }
   }
